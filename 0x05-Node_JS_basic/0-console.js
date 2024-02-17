1db0f3640a67533fc16334870484939cb67d01c3
TebogoLegoabe <tebogolegoab5@gmail.com>
2024-02-17 17:39:45 +0200

    updated

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,8 @@
-#!/usr/bin/env node
-
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ */
 const displayMessage = (msg) => {
   console.log(msg);
 };
